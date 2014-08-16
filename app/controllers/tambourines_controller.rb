59d17ea5a032ba06fa4fa4e59b28858d8b002db9
meteorakey <meteorakey@gmail.com>
2014-08-16 11:28:44 +0900

    make parser module

--- app/controllers/tambourines_controller.rb
+++ app/controllers/tambourines_controller.rb
@@ -23,5 +23,25 @@ class TambourinesController < ApplicationController
     params.require(:illust).permit(:title, :date)
   end
 
+end
 
+module Parser
+  def parse_json(json)
+    # json の型をチェック
+    #puts json.class
+
+    # json を hash にする
+    hash = json.parse()
+
+    # results に画像の hash データを格納
+    results = []
+    posts = hash['response']
+    posts.each do |post|
+      photos = post['photos']
+      photos.each do |photo|
+        results.push(photo['original_size'])
+      end
+    end
+    return results
+  end
 end
